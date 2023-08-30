f1a3d5fa8771147ac1057e83618833e7e956c783
Yusuf <yusufjana18@gmail.com>
2023-08-30 20:24:57 +0700

    commit

---
+++ index.js
@@ -0,0 +1,79 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const display = document.querySelector(".w-full .bg-[#FCDFE5] .text-4xl");
+    const buttons = document.querySelectorAll(".button");
+    let currentInput = "";
+    let previousInput = "";
+    let operator = "";
+
+    buttons.forEach((button) => {
+        button.addEventListener("click", () => {
+            const buttonText = button.textContent;
+
+            if (buttonText === "AC") {
+                clear();
+            } else if (buttonText === "C") {
+                currentInput = currentInput.slice(0, -1);
+                updateDisplay();
+            } else if (buttonText === "=") {
+                calculate();
+            } else if (/[0-9]/.test(buttonText)) {
+                currentInput += buttonText;
+                updateDisplay();
+            } else {
+                handleOperator(buttonText);
+            }
+        });
+    });
+
+    function updateDisplay() {
+        display.textContent = currentInput;
+    }
+
+    function clear() {
+        currentInput = "";
+        previousInput = "";
+        operator = "";
+        updateDisplay();
+    }
+
+    function handleOperator(op) {
+        if (currentInput !== "") {
+            if (previousInput !== "") {
+                calculate();
+            }
+            operator = op;
+            previousInput = currentInput;
+            currentInput = "";
+        }
+    }
+
+    function calculate() {
+        if (previousInput !== "" && currentInput !== "" && operator !== "") {
+            const prev = parseFloat(previousInput);
+            const current = parseFloat(currentInput);
+            let result;
+
+            switch (operator) {
+                case "+":
+                    result = prev + current;
+                    break;
+                case "-":
+                    result = prev - current;
+                    break;
+                case "x":
+                    result = prev * current;
+                    break;
+                case "/":
+                    result = prev / current;
+                    break;
+                default:
+                    return;
+            }
+
+            currentInput = result.toString();
+            previousInput = "";
+            operator = "";
+            updateDisplay();
+        }
+    }
+});
